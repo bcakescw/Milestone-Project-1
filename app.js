7b8a291af24684b260fe1b38b559e42dee312947
Courtney Carroll <bcakescw@gmail.com>
2022-06-20 19:18:07 -0400

    comment edited

--- app.js
+++ app.js
@@ -2,6 +2,3 @@ document.addEventListener('DOMContentLoaded', () => {
     //card options
 
 })
-
-// Code with Ania Kubow: used to help write javascript for game
-//https://www.youtube.com/watch?v=tjyDOHzKN0w
