6a3a0360bece0bd3ed86165f0f51f4688275c261
Courtney Carroll <bcakescw@gmail.com>
2022-06-20 20:31:51 -0400

    variables assigned and images assigned in arrary with a function

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
-document.addEventListener('DOMContentLoaded', () => {
-    //card options
+//selects the section part of the html and assigns it to section
+const section = document.querySelector("section");
 
-})
+// selects the span on the html and assigns it to result
+const result = document.querySelector("span");
+
+//You get 12 chances to match up all the images
+const matchingChances = 12;
+
+//link text 'results' to matchingChances
+result.textContent = matchingChances;
+
+//generate images and name of images
+const getData = function() {[
+    { imgSrc: "./images/AnemometerPic.jpeg", name: "anemometer picture"},
+    { imgSrc: "./images/Anemometer.jpeg", name: "anemometer definition"},
+    { imgSrc: "./images/Barometer.jpeg", name: "barometer definition"},
+    { imgSrc: "./images/mercury-barometer-science-photo-library.jpeg", name: "barometer picture"},
+    { imgSrc: "./images/Rain-gauge.jpeg", name: "rain gauge picture"},
+    { imgSrc: "./images/rain-gauge.jpeg", name: "rain gauge defintion"},
+    { imgSrc: "./images/thermometer.jpeg", name: "thermometer picture"},
+    { imgSrc: "./images/Thermometer.jpeg", name: "thermometer defintion"},
+]}
